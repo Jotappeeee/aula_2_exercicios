6a416258a3f71ff001c7d875ba1e44cf92d96907
Jotappeeee <145858432+Jotappeeee@users.noreply.github.com>
2023-09-23 17:17:44 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+# crie um sistema que mostre um texto na tela
+
+# print('---------------------------')
+# name = str(input('digite seu nome: '))
+# # print('seu nome é:',name)
+# # print('---------------------------')
+
+# # # if e else
+
+# # print('---------------------------')
+# # num1 = int(input("Digite o numero: "))
+# # num2 = int(input("Digite o numero: "))
+# # if num1 == num2:
+# #   print('É Igual')
+# # else:
+# #   print('É Diferente')
+# # print('---------------------------')
+
+
+#--------------------------------------------------------------------------------------------
+
+#**1  -  CRIE UM SISTEMA QUE SOME 2  NUMEROS ***
+
+print(' ')
+print('--------------CALCULADORA DE SOMA------------------')
+number1 = int(input('digite um numero: '))
+number2 = int(input('digite mais um numero: '))
+sum = number1+number2
+print('A soma desses numeros é: ',sum)
+print('---------------------------------------------------')
+print(' ')
+
+
+# **2 - CRIE UM SISTEMA QUE IMPRIMA SEU NOME NA TELA** 
+
+print('------------------------------------------------------------')
+name = "João"
+print('Ola, bem vindo', name)
+print('------------------------------------------------------------')
+print(' ')
+
+# **3 - CRIE UM SISTEMA QUE IMPRIMA SEU NOME E UM CUMPRIMENTO APÓS nome(CONCATENE)**
+
+print('------------------------------------------------------------')
+nam = str(input('Digite o seu nome: '))
+print('Ola',nam+', Tudo bem?')
+print('------------------------------------------------------------')
+print(' ')
+
+# **4 - CRIE UM SISTEMA QUE MULTIPLIQUE 2 NÚMEROS**
+
+print('--------------CALCULADORA DE MULTIPLICAÇÃO------------------')
+num1 = int(input('digite um numero: '))
+num2 = int(input('digite mais um numero: '))
+mult = num1*num2
+print('O produto desses numeros é: ',mult)
+print('------------------------------------------------------------')
